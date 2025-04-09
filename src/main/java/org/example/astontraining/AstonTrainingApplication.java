14e5c229e152ea2c5b556691a770eee7ea406cfa
Sergey Rogozhkin <r8damik@gmail.com>
2025-04-09 12:44:18 +0300

    first commit

---
+++ src/main/java/org/example/astontraining/AstonTrainingApplication.java
@@ -0,0 +1,13 @@
+package org.example.astontraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstonTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstonTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/astontraining/AstonTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.astontraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstonTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
